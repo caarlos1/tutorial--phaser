1eeb1b9ed793f012b8599980f7b6e7bb6e661932
Carlos Roberto <crpu4a@gmail.com>
2022-06-17 18:52:23 -0300

    Progresso até o modulo 7

--- src/index.js
+++ src/index.js
@@ -3,6 +3,10 @@ import Phaser from "phaser";
 import { skyImg, groundImg, starImg, bombImg, dudeImg } from "./assets";
 
 class MyGame extends Phaser.Scene {
+    platforms;
+    player;
+    cursors;
+
     constructor() {
         super();
     }
@@ -20,7 +24,67 @@ class MyGame extends Phaser.Scene {
 
     create() {
         this.add.image(0, 0, "sky").setOrigin(0, 0);
-        this.add.image(400, 300, "star");
+
+        this.platforms = this.physics.add.staticGroup();
+
+        this.platforms.create(400, 568, "ground").setScale(2).refreshBody();
+        this.platforms.create(600, 400, "ground");
+        this.platforms.create(50, 250, "ground");
+        this.platforms.create(750, 220, "ground");
+
+        this.player = this.physics.add.sprite(100, 450, "dude");
+
+        this.player.setBounce(0.2);
+        this.player.setCollideWorldBounds(true);
+
+        // this.player.setGravityY(300);
+
+        this.physics.add.collider(this.player, this.platforms);
+
+        this.anims.create({
+            key: "left",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 0,
+                end: 3,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.anims.create({
+            key: "turn",
+            frames: [{ key: "dude", frame: 4 }],
+            frameRate: 20,
+        });
+
+        this.anims.create({
+            key: "right",
+            frames: this.anims.generateFrameNumbers("dude", {
+                start: 5,
+                end: 8,
+            }),
+            frameRate: 10,
+            repeat: -1,
+        });
+
+        this.cursors = this.input.keyboard.createCursorKeys();
+    }
+
+    update() {
+        if (this.cursors.left.isDown) {
+            this.player.setVelocityX(-160);
+            this.player.anims.play("left", true);
+        } else if (this.cursors.right.isDown) {
+            this.player.setVelocityX(160);
+            this.player.anims.play("right", true);
+        } else if (this.player.body.touching.down || this.cursors.down.isDown) {
+            this.player.setVelocityX(0);
+            this.player.anims.play("turn", true);
+        }
+
+        if (this.cursors.up.isDown && this.player.body.touching.down) {
+            this.player.setVelocityY(-200);
+        }
     }
 }
 
@@ -29,6 +93,13 @@ const config = {
     parent: "phaser-example",
     width: 800,
     height: 600,
+    physics: {
+        default: "arcade",
+        arcade: {
+            gravity: { y: 300 },
+            debug: false,
+        },
+    },
     scene: MyGame,
 };
 
